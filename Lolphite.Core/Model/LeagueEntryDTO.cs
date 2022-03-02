9a028883ab8dd5f02adc61489ad37c04323279a3
z4kon <99132335+z4kon@users.noreply.github.com>
2022-03-03 01:01:08 +0200

    Update LeagueEntryDTO.cs

--- Lolphite.Core/Model/LeagueEntryDTO.cs
+++ Lolphite.Core/Model/LeagueEntryDTO.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lolphite.Core.Model
 {
     public class LeagueEntryDTO
